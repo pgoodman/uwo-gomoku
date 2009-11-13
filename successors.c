2507a54b5fc26fdece3a73321e8d76aae545896e
peter.goodman <peter.goodman@5361f0be-c118-11de-8cd0-f941f5a20a08>
2009-11-13 02:29:16 +0000

    LOL I had the wrong name down on the license.

--- successors.c
+++ successors.c
@@ -8,14 +8,16 @@
 
 #include "successors.h"
 
+static player_t which_player;
+
 /**
  * Compare the importance ratings of two cells.
  */
 static int compare_cell(const void *a, const void *b) {
     board_cell_t *bb = (*(board_cell_t **) b);
     board_cell_t *aa = (*(board_cell_t **) a);
-    return (bb->rating[0] + bb->rating[1] + bb->rating[2])
-         - (aa->rating[0] + aa->rating[1] + aa->rating[2]);
+    return (bb->rating[0] + bb->rating[which_player])
+         - (aa->rating[0] + aa->rating[which_player]);
 }
 
 /**
@@ -24,7 +26,9 @@ static int compare_cell(const void *a, const void *b) {
  *
  * This function also returns the average of all importance ratings.
  */
-void gen_successors(board_t *board, ordered_cell_seq_t *seq) {
+void gen_successors(board_t *board,
+                    ordered_cell_seq_t *seq,
+                    const player_t player_id) {
 
     int len = 0;
     board_cell_t *all[BOARD_NUM_CELLS];
@@ -43,6 +47,7 @@ void gen_successors(board_t *board, ordered_cell_seq_t *seq) {
     }
 
     /* sort the cell pointers according to the cell importance rating */
+    which_player = player_id;
     qsort(
         &(all[0]),
         (size_t) len,
